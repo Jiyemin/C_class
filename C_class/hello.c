1d209e02b307afccbd32731aa260ca957068b067
jiyemin <jym21462@gmail.com>
2025-01-09 16:49:05 +0900

    day2

--- C_class/hello.c
+++ C_class/hello.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 int main(void) {
 	printf("hello \n");
     return 0;
---
+++ C_class/변수.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(void)
+{
+	// 한줄 주석
+	/*
+	여
+	러
+	줄
+	주
+	석
+	*/
+
+	int num = 3; //선언과 초기화 동시
+	// 자료형 변수명 = 값;
+	//자료형 변수명2; //선언
+	//변수명2 =3; // 초기화
+	//변수명 3 = 안됨!
+
+	int num2; //선언
+	num2 = 2; //초기화
+    num2 = 5; //초기화
+
+	int number1, number2; // 선언
+	number1 = 10; //number1 초기화
+	number2 = 20; //number2 초기화
+
+	int number3 = 30, number4 = 40;
+
+   //변수명 규칙
+	int num = 50;
+	int number16 = 16; //숫자조합 가능
+	int number_20 = 20; //언더바 가능
+	int Number = 165; //대문자 가능
+
+	//불가능한 변수명
+   
+
+
+	
+
+
+	return 0;
+
+
+	return 0;
+}
