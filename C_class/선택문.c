3a553e8f261c5291c1b177e91f29458d850d63b4
jiyemin <jym21462@gmail.com>
2025-01-17 16:47:25 +0900

    day8

---
+++ C_class/선택문.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*
+	int 변수명 = 초기값;
+	switch(변수명){
+	case 값 1 : 코드 1;
+	break;
+	case 값 2 : 코드 1;
+	break;
+	case 값 3 : 코드 1;
+	break;
+	case 값 4 : 코드 1;
+	break;
+	defult : 코드;
+	beak;
+	}
+	*/
+
+	int day;
+	printf("요일을 선택하세요. (1 ~ 7 중 택1) : ");
+	scanf_s("%d", &day);
+
+	switch (day) {
+	case 1 : printf("월요일입니다. \n");
+		break;
+	case 2 : printf("화요일입니다. \n");
+		break;
+	case 3 : printf("수요일입니다. \n");
+		break;
+	case 4 : printf("목요일입니다. \n");
+		break;
+	case 5 : printf("금요일입니다. \n");
+		break;
+	case 6 : printf("토요일입니다. \n");
+		break;
+	case 7 : printf("일요일입니다. \n");
+		break;
+
+	default: printf("유효하지 않은 입력입니다. (1~7 중에 입력해주세요)");
+	}
+	
+	return 0;
+}
